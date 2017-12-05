11d9a7f75242790aa2a3db1459bea5acaf51a7e5
Ethaning <ethanjcarter@hotmail.co.uk>
2017-12-05 23:21:27 +0000

    drafted xs screen width navigation.

---
+++ app/assets/javascripts/navigation.js
@@ -0,0 +1,6 @@
+function showNav() {
+  document.getElementById("nav-container").classList.toggle("active");
+  document.getElementById("menu-icon").classList.toggle("transparent");
+  document.getElementById("exit-menu-icon").classList.toggle("opaque");
+}
+
