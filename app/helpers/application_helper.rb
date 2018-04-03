767695c378e32fedb7f410fe23577ad9b1e76566
Ethaning <ethanjcarter@hotmail.co.uk>
2018-04-03 23:26:24 +0100

    admin must sign in before accessing admin area

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def current_page_testimonials?
-    current_page? action: 'testimonials'
+    controller.action_name == 'testimonials' && controller.controller_name == 'home'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
 
   get 'mail', to: 'home#mail'
