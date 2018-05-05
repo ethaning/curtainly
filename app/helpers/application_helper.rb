706bf7686ac421aea4efc21ee535e5d812297776
Ethaning <ethanjcarter@hotmail.co.uk>
2018-05-05 15:05:33 +0100

    added border under logo for non homepage sections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_page_testimonials?
     controller.action_name == 'testimonials' && controller.controller_name == 'home'
   end
+
+  def current_page_about_us?
+    controller.action_name == 'about_us' && controller.controller_name == 'home'
+  end
+
+  def non_homepage_section?
+    current_page_testimonials? || current_page_about_us?
+  end
 end
